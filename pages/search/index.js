36424d2be1d7e0fc2f60aa55204c7e052c2ea307
Your Name <you@example.com>
2019-08-08 15:02:48 +0800

    完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,51 @@
 // pages/search/index.js
+import regeneratorRuntime, { async } from "../../lib/runtime/runtime";
+import { request } from "../../request/index.js";
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: [],
+    kong: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  timeId: -1,
+  handeSearchInput(e) {
+    // console.log(e);
+    const { value } = e.detail;
+    console.log(value);
+    this.setData({
+      kong:value
+    })
+    if(this.data.kong==''){
+      this.setData({
+        goods: []
+      });
+    }
+    if (!value.trim()) {
+      return;
+    }
+    clearTimeout(this.timeId);
+    this.timeId = setTimeout(() => {
+      this.getQsearch(value);
+    }, 1000);
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handlekong() {
+    this.setData({
+      kong: "",
+      goods: []
+    });
   },
+  async getQsearch(query) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    const goods = await request({
+      url: "/goods/qsearch",
+      data: { query }
+    });
+    this.setData({
+      goods
+    });
   },
+});
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
